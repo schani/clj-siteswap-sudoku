e2f1b83d85fa18feaec4e67d1cc631a830d3a6f9
Mark Probst <mark.probst@gmail.com>
2010-06-28 04:31:20 +0200

    Use Clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject siteswap-sudoku "0.0.1-SNAPSHOT"
   :description "Siteswap Sudoku Generator"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
