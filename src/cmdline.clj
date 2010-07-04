fa429696ee5ee29f25f7e30f34e2e342cbddbad1
Mark Probst <mark.probst@gmail.com>
2010-07-04 21:50:53 +0200

    Maximum number of tries in command line interface.

--- src/cmdline.clj
+++ src/cmdline.clj
@@ -11,20 +11,24 @@
      [min-throw "Minimum throw" "1"]
      [max-throw "Maximum throw" "9"]
      [unknowns "Number of unknowns" "5"]
+     [max-tries "Maximum number of tries" nil]
      remaining]
-    (loop []
-      (let [min-throw (java.lang.Integer/parseInt min-throw)
-	    max-throw (java.lang.Integer/parseInt max-throw)
-	    sudoku (make-siteswap-sudoku (java.lang.Integer/parseInt rows)
-					 (java.lang.Integer/parseInt cols)
-					 min-throw
-					 max-throw
-					 (java.lang.Integer/parseInt unknowns))]
-	(if sudoku
-	  (do
-	    (println (sudoku-to-string sudoku))
-	    (println)
-	    (println (sudoku-to-string (solve-sudoku sudoku min-throw max-throw))))
-	  (do
-	    (println "No luck - retrying")
-	    (recur)))))))
+    (let [min-throw (java.lang.Integer/parseInt min-throw)
+	  max-throw (java.lang.Integer/parseInt max-throw)
+	  max-tries (and max-tries (java.lang.Integer/parseInt max-tries))]
+      (loop [tries 1]
+	(let [sudoku (make-siteswap-sudoku (java.lang.Integer/parseInt rows)
+					   (java.lang.Integer/parseInt cols)
+					   min-throw
+					   max-throw
+					   (java.lang.Integer/parseInt unknowns))]
+	  (if sudoku
+	    (do
+	      (println (sudoku-to-string sudoku))
+	      (println)
+	      (println (sudoku-to-string (solve-sudoku sudoku min-throw max-throw))))
+	    (if (and max-tries (>= tries max-tries))
+	      (println "No puzzles found")
+	      (do
+		(println "No luck - retrying after" tries "tries")
+		(recur (inc tries))))))))))
